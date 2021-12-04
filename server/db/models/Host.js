777d07f76eb7553bcf747c392decc4236c0c5c1a
Greg <gdbabbert@gmail.com>
2021-12-04 12:11:28 -0500

    uncomment jwt

--- server/db/models/Host.js
+++ server/db/models/Host.js
@@ -1,6 +1,8 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const axios = require("axios");
 
 const SALT_ROUNDS = 5;
 
