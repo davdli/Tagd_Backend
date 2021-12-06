d26f3e5542dde1dac0e56175e75ce64f1073e72f
Greg <gdbabbert@gmail.com>
2021-12-06 11:33:07 -0500

    work to fix post routes

--- server/api/hosts.js
+++ server/api/hosts.js
@@ -3,15 +3,24 @@ const Sequelize = require('sequelize');
 const { models: { Host } } = require('../db');
 
 // all hosts
-router.get( '/', async (req, res, next) => {
-  try {
-    const hosts = await Host.findAll({
-      attributes: ['firstName', 'lastName', 'email']
-    });
-    res.send(hosts);
-  } catch (error) {
-    next(error);
-  }
+router.route('/')
+  .get(async (req, res, next) => {
+    try {
+      const hosts = await Host.findAll({
+        attributes: ['firstName', 'lastName', 'email']
+      });
+      res.send(hosts);
+    } catch (error) {
+      next(error);
+    }
+  }).post(async (req, res, next) => {
+    try {
+      const newHost = await Host.create(req.body);
+      res.status(200).json(newHost);
+    } catch (error) {
+      next(error);
+    }
+  });
 
 router.route('/:id')
   .get(async (req, res, next) => {
@@ -41,32 +50,24 @@ router.route('/:id')
   })
   .put(async (req, res, next) => {
     try {
-      const {firstName, lastName, email} = req.body
-      const update = {firstName, lastName, email}
+      const { firstName, lastName, email } = req.body
+      const update = { firstName, lastName, email }
       const host = await Host.findByPk(req.params.id)
 
-      if(host){
-      await host.update(update)
-      const newHost = await Host.findOne({
-        where: {id: req.params.id},
-        attributes: ["id", "firstName", "lastName", "email"]
-      })
-      res.send(newHost)
-    }else {
-      res.sendStatus(404)
-    }
+      if (host) {
+        await host.update(update)
+        const newHost = await Host.findOne({
+          where: { id: req.params.id },
+          attributes: ["id", "firstName", "lastName", "email"]
+        })
+        res.send(newHost)
+      } else {
+        res.sendStatus(404)
+      }
     } catch (error) {
       next(error)
     }
   })
-  .post(async(req, res, next) => {
-    try {
-      const newHost = await Host.create(req.body);
-      res.status(200).json(newHost);
-    } catch (error) {
-      next(error);
-    }
-  });
 })
 
 module.exports = router;
